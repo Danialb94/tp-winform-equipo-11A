8c2c74a51ee3b9ea76119e12b0df1f4b820572ce
AbrildelaIglesia <abixdlie@gmail.com>
2025-09-07 07:56:48 -0300

    Agrego la clase Marca

---
+++ dominio/Marca.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Marca
+    {
+        public int IdMarca { get; } // Porque en DB el valor del IdMarca tiene la propiedad 'IDENTITY'
+
+        public string Descripcion { get; set; }
+        
+    }
+    
+}
