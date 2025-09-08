327b1fe3d70c532f520f0e0664ca8fd257d17e63
AbrildelaIglesia <abixdlie@gmail.com>
2025-09-08 18:40:44 -0300

    ArticuloNegocio, me faltó agregar esta novedad!

--- negocio/ArticuloNegocio.cs
+++ negocio/ArticuloNegocio.cs
@@ -1,10 +1,10 @@
-﻿using dominio;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using dominio;
 
 namespace negocio
 {
