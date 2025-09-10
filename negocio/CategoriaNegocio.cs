5d1fcb6a55cdc7b7684ee654d06d9bae534976e2
Maruchan <mariannai99@icloud.com>
2025-09-10 14:10:48 -0300

    Implementación inicial de CategoriaNegocio con listar categorías

--- negocio/CategoriaNegocio.cs
+++ negocio/CategoriaNegocio.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using dominio;
 
 namespace negocio
 {
     public class CategoriaNegocio
     {
+        public List<Categoria> listar()
+        {
+            List<Categoria> lista = new List<Categoria>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT Id, Descripcion FROM CATEGORIAS");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Categoria cat = new Categoria();
+                    cat.IDCategoria = (int)datos.Lector["IDCategoria"];
+                    cat.Descripcion = (string)datos.Lector["Descripcion"];
+                    listar().Add(cat);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            finally
+            {
+                datos.cerrarConexion();
+            }
+
+        }
     }
 }
